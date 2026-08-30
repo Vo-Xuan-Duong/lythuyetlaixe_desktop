22ac4e0421a5e25b757082f7d56b43c057933eff
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 23:11:06 +0700

    feat: expose questions by license for exam engine

--- src/domain/repositories/QuestionRepository.ts
+++ src/domain/repositories/QuestionRepository.ts
@@ -1,8 +1,9 @@
-import type { Question } from "../entities/question";
+import type { LicenseType, Question } from "../entities/question";
 
 export interface QuestionRepository {
   count(): Promise<number>;
   getById(id: number): Promise<Question | null>;
   listByCategory(categoryCode: string): Promise<Question[]>;
   listCritical(): Promise<Question[]>;
+  listForLicense(licenseType: LicenseType): Promise<Question[]>;
 }
