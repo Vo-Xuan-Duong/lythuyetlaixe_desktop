1a8c45487ed7dd610e8028184173ccfe21fbfdd0
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 23:03:11 +0700

    style: load question collection styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app/App";
 import "./styles/global.css";
 import "./styles/learning-session.css";
 import "./styles/learning-catalog.css";
+import "./styles/question-collection.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
