c83914abdfbd3f6978bcf1c00f5505474431bc1a
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 23:00:34 +0700

    style: load learning catalog styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/global.css";
 import "./styles/learning-session.css";
+import "./styles/learning-catalog.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
