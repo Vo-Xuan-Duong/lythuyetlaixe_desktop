6800006e5457a2ff0af607f45b337287ec748636
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-31 08:02:35 +0700

    feat: register filesystem plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -99,6 +99,7 @@ fn migrations() -> Vec<Migration> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations(DATABASE_URL, migrations())
