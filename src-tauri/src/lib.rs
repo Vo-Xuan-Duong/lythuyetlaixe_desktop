c1b0842177aea5a5378f1ee50d5dbc10e0edd8f4
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-31 09:48:15 +0700

    feat: initialize notification plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -100,6 +100,7 @@ fn migrations() -> Vec<Migration> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(
             tauri_plugin_sql::Builder::default()
